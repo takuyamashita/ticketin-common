4818562168df0da4cd8aa303a42647cae8268dc4
takuyamashita <taku.pro-myfam@ezweb.ne.jp>
2021-03-14 23:32:11 +0900

    update config

---
+++ src/middlewares/error-handler.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import { CustomError } from '../errors/custom-error';
+
+export const errorHandler = (
+    error: Error,
+    request: Request,
+    response: Response,
+    next: NextFunction,
+) => {
+
+    if( error instanceof CustomError){
+        return response.status(error.statusCode).send({errors: error.serializeErrors()});
+    }
+
+    response.status(400).send([{message: 'Something went wrong'}]);
+};
