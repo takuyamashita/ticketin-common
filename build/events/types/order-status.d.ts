932b4a47f3e165103af0bc3ec7bff42f032c4d77
takuyamashita <taku.pro-myfam@ezweb.ne.jp>
2021-03-24 15:23:11 +0900

    common

---
+++ build/events/types/order-status.d.ts
@@ -0,0 +1,6 @@
+export declare enum OderStatus {
+    Created = "created",
+    Cancelled = "cancelled",
+    AwaitingPayment = "awaiting:payment",
+    Complete = "complete"
+}
---
+++ build/events/types/order-status.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.OderStatus = void 0;
+var OderStatus;
+(function (OderStatus) {
+    OderStatus["Created"] = "created";
+    OderStatus["Cancelled"] = "cancelled";
+    OderStatus["AwaitingPayment"] = "awaiting:payment";
+    OderStatus["Complete"] = "complete";
+})(OderStatus = exports.OderStatus || (exports.OderStatus = {}));
