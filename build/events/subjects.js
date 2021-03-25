592ac0930b9302a765b6bc24a91e3e0dfdf3d461
takuyamashita <taku.pro-myfam@ezweb.ne.jp>
2021-03-25 15:17:53 +0900

    common

--- build/events/subjects.js
+++ build/events/subjects.js
@@ -5,5 +5,7 @@ var Subjects;
 (function (Subjects) {
     Subjects["TicketCreated"] = "ticket:created";
     Subjects["TicketUpdated"] = "ticket:updated";
+    Subjects["OrderCreated"] = "order:created";
+    Subjects["OrderCancelled"] = "order:cancelled";
 })(Subjects = exports.Subjects || (exports.Subjects = {}));
 ;
